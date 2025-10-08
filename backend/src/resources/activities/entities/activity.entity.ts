7013cfe36bab1d6b185d594505be94695326392c
mgn12 <marcosnuero11@gmail.com>
2025-10-08 17:30:35 +0200

    Add ORM entities

--- backend/src/resources/activities/entities/activity.entity.ts
+++ backend/src/resources/activities/entities/activity.entity.ts
@@ -1,1 +1,19 @@
-export class Activity {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Activity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  sets: number;
+
+  @Column()
+  reps: number;
+
+  @Column()
+  weight: number;
+
+  @Column('int', { array: true })
+  results: number[];
+}
