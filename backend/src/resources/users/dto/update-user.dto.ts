7b88c5a8179155ef6ea0767d21313af5268d571a
mgn12 <marcosnuero11@gmail.com>
2025-10-23 16:58:23 +0200

    user fixed

--- backend/src/resources/users/dto/update-user.dto.ts
+++ backend/src/resources/users/dto/update-user.dto.ts
@@ -1,7 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
 
-// Usaremos PartialType cuando queramos actualizar un objeto o solo unos atributos de su objeto
-// de esta manera no tendremos obligacion de actualizar el objeto entero sino los atributos que queramos
-// y no se nos exigiran todos
 export class UpdateUserDto extends PartialType(CreateUserDto) {}
